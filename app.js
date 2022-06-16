529ba5d63cb019cc03dfe33a5aeae27b0e84a31f
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-06-16 13:31:53 -0700

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ function descendingOrder(n){
 /*conclusion: here i started with a general plan because i had no immediate thought on how to complete this task. I broke it down into bigger chunks to be broken down ino smaller chunks. I practiced using .join(), and used codepen to do my own testing when needed. */
 
 //#2
+
 /*You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.*/
 function findOutlier(integers){
   const evenNums = integers.filter(num => num % 2 === 0);
@@ -29,3 +30,19 @@ function findOutlier(integers){
 	}
 //console.log(findOutlier([2, 4, 6, 8, 5]))
 /*Conclusion: Here I learned to use Number() to turn number array elements into a number. I also learned that a switch is seeing if the case is equal to the switch expression.*/
+
+/* #3
+
+I was supposed t o be able to take an array of words and whichhever words were five letters or longer i had to retern it reversed.*/
+function spinWords(string){
+  //split the string into an array, use .map, the calling function will check if the length of the word is 5 or more, if yes it will return thr word reversed, if not it just returnd the word. 
+	let arr = string.split(" ");
+	const resultArr = arr.map(word => word.length < 5 ? word : word.split("").reverse().join(""))
+	 return resultArr.join(" ");
+}
+
+console.log(spinWords("Hey fellow warriors"))
+// conclusion: learned about ternary operators, and wrote my code in a way that I seen on codewars, which was chaining my methods and writing shorter solutions.
+
+// June 16, 2022
+//
