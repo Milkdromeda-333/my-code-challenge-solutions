ede75efb571b0ae3d960cf334c097d4baa4b3f62
Anjanique M <99520247+Milkdromeda-333@users.noreply.github.com>
2022-06-18 14:23:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* June 15, 2022
 
+
 #1 
 Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. 
 Essentially, rearrange the digits to create the highest possible number.*/
@@ -16,8 +17,8 @@ function descendingOrder(n){
 /*conclusion: here i started with a general plan because i had no immediate thought on how to complete this task. 
 I broke it down into bigger chunks to be broken down ino smaller chunks. I practiced using .join(), and used codepen to do my own testing when needed. */
 
-//#2
 
+//#2
 /*You are given an array (which will have a length of at least 3, but could be very large) containing integers. 
 The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. 
 Write a method that takes the array as an argument and returns this "outlier" N.*/
@@ -35,8 +36,8 @@ function findOutlier(integers){
 //console.log(findOutlier([2, 4, 6, 8, 5]))
 /*Conclusion: Here I learned to use Number() to turn number array elements into a number. I also learned that a switch is seeing if the case is equal to the switch expression.*/
 
-/* #4
 
+/* #4
 I was supposed t o be able to take an array of words and whichhever words were five letters or longer i had to retern it reversed.*/
 function spinWords(string){
   //split the string into an array, use .map, the calling function will check if the length of the word is 5 or more, if yes it will return thr word reversed, if not it just returnd the word. 
@@ -49,6 +50,7 @@ console.log(spinWords("Hey fellow warriors"))
 // conclusion: learned about ternary operators, and wrote my code in a way that I seen on codewars, which was chaining my methods and writing shorter solutions.
 
 // June 16, 2022
+
 
 //#5
 /*You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. 
@@ -74,6 +76,7 @@ function lessThanFour(names){
 //console.log(likes(['Alex', 'Jacob', 'Mark', 'William', 'Illumi', 'Valencia']))
 // conclusion: This one was a little more challenging for me. I figured out htat if there isnt a short way to do it, figure out how to do it the long way.
 
+
 // #6
 /* A Narcissistic Number is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base.
 In this Kata, we will restrict ourselves to decimal (base 10).*/
@@ -88,3 +91,30 @@ function narcissistic(value) {
 }
 console.log(narcissistic(153))
 // conclusion: very happy with my use of the ternary operator, AND reduce here. And, I learned about Math.pow().
+
+
+// June 18, 2022
+
+
+// #7
+// Given an array of arrays where the first element in the nested array was people getting on, and the second was those getting off, I had to calculate how many peopel were still on the bus.
+var number = function (busStops) {
+	/*maybe have a counter inside the function that starts at zero. Write a for statement that adds the first element of the array and subtracts the second element. and after the loop return the counter.*/
+	let numOfPassengers = 0;
+	arr.forEach(function(passengers){
+		numOfPassengers += passengers[0];
+		numOfPassengers -= passengers[1];
+	})
+	return numOfPassengers;
+};
+//below is refactored to utilize and practice using array reduce method.
+var number = function (busStops) {
+	return busStops.reduce(
+		(peopleOnTheBus, stop) => peopleOnTheBus + stop[0] - stop[1], 0);
+};
+console.log(number([
+	[10, 0],
+	[3, 5],
+	[5, 8]
+]));
+/* conclusion: AFter a couplr of faile attmepts of codewars cahllenges I finally succeeded with one. Very happy with how fast i figured it out.
